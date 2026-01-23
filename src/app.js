c542245050c1b25eb527dc33b9082f38110ab1db
pallavbharti <pallavbharti8795@gmail.com>
2026-01-23 17:32:44 +0530

    route practices

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
-app.get('/',(req,res)=>{
+
+app.get('/a/',(req,res)=>{
+    res.send({name:"John Doe", age:30});
+});
+app.get('/user',(req,res)=>{
+    console.log(req.query);
+    res.send({name:"John Doe", age:30});
+});
+
+
+app.get("/user/:id/:password/:age",(req,res)=>{
+    console.log(req.params);
+    res.send("User created");
+});
+
+app.post('/user',(req,res)=>{
+    res.send("User created");
+});
+app.delete('/user',(req,res)=>{
+    res.send("User deleted");
+})
+app.use('/',(req,res)=>{
     res.send("hello world");
-});
-app.get('/profile',(req,res)=>{
-    res.send("Welcome to your profile page");
-});
-app.get('/test',(req,res)=>{
-    res.send("No test cases are assigned yet");
-});
-app.get('/contact',(req,res)=>{
-    res.send("contact us at");
 });
 app.listen(7777,()=>{
     console.log("Server is running on port 7777");
