a8732bb0618fe1cfd94097df9e6395f198469f12
pallavbharti <pallavbharti8795@gmail.com>
2026-02-02 11:08:02 +0530

    dynamic signup using postman

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
-
 const User = require('./models/user');
 
+app.use(express.json());
 
 app.post('/signup',async(req,res)=>{
+    
     // Signup logic here
-    const user = new User({
-    firstName:"virat",
-    lastName: "kohli",
-    email: "virat@kohli.com",
-    password: "virat123",
-   });
+    const user = new User(req.body);
+    try{
     await user.save();
-    res.send("User signed up successfully");
+    res.status(200).send({message:"User registered successfully"});
+    }catch(err){
+        res.status(400).send({message:"Error registering user", error:err.message});
+    }
 });
 
 connectDB().then(()=>{
--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ const mongoose = require ('mongoose');
 // Connect to MongoDB
 const connectDB = async()=>{
     await mongoose.connect("mongodb+srv://pallavbharti:devTinder@cluster0.xwzdlxd.mongodb.net/devTinder");
+    };
 
-}
+
 module.exports= connectDB;
